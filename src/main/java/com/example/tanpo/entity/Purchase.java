7527abfb10a3189b54f2e9dbfa5e1d9f1a46123b
DESKTOP-CL839OA\dlxksdnr <pp990417@naver.com>
2024-10-03 11:56:36 +0900

    lombok 빌드 오류 해결 후 데이터베이스 전송까지 확인

--- src/main/java/com/example/tanpo/entity/Purchase.java
+++ src/main/java/com/example/tanpo/entity/Purchase.java
@@ -7,9 +7,9 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "purchase")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Data // 롬복이 Getter, Setter, toString, equals, hashCode 메서드를 자동으로 생성
+@NoArgsConstructor // 기본 생성자 자동 생성
+@AllArgsConstructor // 모든 필드를 초기화하는 생성자 자동 생성
 public class Purchase {
 
     @Id
